8606d9592736ae5a25a2be9d96aa65a8ad8c55f8
eric feigner <feigner@gmail.com>
2018-01-30 18:17:15 -0800

    update pep8 linter to flake8

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ DOTFILES=$HOME/dotfiles
 ln -s $DOTFILES/zsh/zshrc $HOME/.zshrc
 ln -s $DOTFILES/git/gitconfig $HOME/.gitconfig
 ln -s $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
+ln -s $DOTFILES/tmux $HOME/.tmux
 ln -s $DOTFILES/alias/aliasrc $HOME/.aliasrc
 ln -s $DOTFILES/vim $HOME/.vim
 ln -s $DOTFILES/vim/vimrc $HOME/.vimrc
